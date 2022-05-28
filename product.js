060001fbbd92ec1b8cae39f4a92582194d240ea1
Norwegian07 <88303771+Norwegian07@users.noreply.github.com>
2022-05-28 10:59:51 +0530

    Add files via upload

---
+++ product.js
@@ -0,0 +1,7 @@
+function openNav(){
+document.getElementById("navcontent-holder").style.width = "100%"
+}
+
+function closeNav(){
+    document.getElementById("navcontent-holder").style.width = "0%"
+    }
